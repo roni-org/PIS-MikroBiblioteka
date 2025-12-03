7217f72426b33bdbed79888ae12512b021965ab8
dianap25 <dianapelin25@gmail.com>
2025-12-03 13:50:58 +0100

    Corrected tests

--- backend/src/main/java/com/mikroBiblioteka/project/repository/FileMetaRepository.java
+++ backend/src/main/java/com/mikroBiblioteka/project/repository/FileMetaRepository.java
@@ -4,6 +4,9 @@ import com.mikroBiblioteka.project.model.FileMeta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FileMetaRepository extends JpaRepository<FileMeta, Long> {
+    Optional<FileMeta> findByName(String file);
 }
